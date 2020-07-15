6be16ff6211c666b393e4ed990d22b33fc76b912
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-15 09:02:38 -0700

    Add files via upload

---
+++ Models/StockDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stock_App.Models
+{
+    public class StockDbContext:DbContext
+    {
+        public StockDbContext(DbContextOptions<StockDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<StockSale> StockSales { get; set; }
+    }
+}
---
+++ Models/StockSale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stock_App.Models
+{
+    public class StockSale
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName="nvarchar(100)")]
+        public string StockName { get; set; }
+        [Column(TypeName="nvarchar(255)")]
+        public string Description { get; set; }
+        [Column(TypeName="decimal(2)")]
+        public decimal PurchasePrice { get; set; }
+        [Column(TypeName="decimal(2)")]
+        public decimal? SalePrice { get; set; }
+        [Column(TypeName="nvarchar(100)")]
+        public string Strategy { get; set;}
+    }
+}
